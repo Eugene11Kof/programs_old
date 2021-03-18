cfab2100fb3c3d96821ef120be3b0c0ab828e80f
Eugene Kozhevnikov <80280392+kofman11@users.noreply.github.com>
2021-03-18 12:14:14 +0300

    Update Номера.py

--- Номера.py
+++ Номера.py
@@ -1,20 +1,25 @@
-# #алгоритм работает для следующих номеров:
+'''
+# алгоритм работает для следующих номеров (файл 'number1.txt'):
 # +79877001505
 # 9855355353
 # +7 937 799 23 12
 # +7(987) 707-30-30
 # 78485584
 # 242552666
-# не работает для номеров:
-# 88005355353    - так как 10 цифр
-# 88005355353 Вася 9 - так как последняя цифра 9 пойдет в итоговый номер
+# 88005355353
+# 88005355353 Вася 9
+# Коля 3 88005355353
+'''
 
-import numpy as np
-f = open('C:\\Users\spams\OneDrive\Рабочий стол\Дворец\\номера.txt','r',encoding = 'utf-8')
-a = [line.strip() for line in f]
-f.close()
-cifra = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-cifra = np.array(cifra, dtype = str)
+file1 = "number1.txt"
+file2 = "number2.txt"
+
+
+with open(file1,'r',encoding = 'utf-8') as f:
+    a = [line.strip() for line in f]
+cifra = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+simbol = ["-"," ","+","(",")","[","]","\t","\r","\n","_",'"',"'"]
+
 b = []
 for i in a:
     count = 0
@@ -23,12 +28,19 @@ for i in a:
             count+=1
         if count == 11:
             b.append(i)
+            break # Чтобы выходил из цикла
+
+    if count == 10:
+        b.append("7"+i) # Добавляем если всего 10 цифр
+
 c = []
 for i in b:
     q = ''
     for j in i:
         if j in cifra:
             q+=j
+        elif not j in simbol and len(q)>=10:
+            break
     if q[0] == '8':
         q = list(q)
         q[0] = '7'
@@ -39,7 +51,6 @@ for i in b:
     c.append('+'+q)
 print(c)
 
-f2 = open('C:\\Users\spams\OneDrive\Рабочий стол\Дворец\\номера_новые.txt', 'w')
-for i in c:
+with open(file2, 'w') as f2:
+    for i in c:
         f2.write(i + '\n')
-
